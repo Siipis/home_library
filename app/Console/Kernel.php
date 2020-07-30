5d6afff3e38feb6f6ae8b413ba6ff28963805eec
Ami Surakka <siipis@live.co.uk>
2020-07-31 00:56:19 +0300

    Added Artisan command for faster production deployment.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,6 +16,7 @@ class Kernel extends ConsoleKernel
         \App\Console\Commands\MakeView::class,
         \App\Console\Commands\MakeLayout::class,
         \App\Console\Commands\MakeView::class,
+        \App\Console\Commands\Production::class,
     ];
 
     /**
